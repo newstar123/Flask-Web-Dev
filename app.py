69801d5e591abab4037511d087561fac6244a4a3
Blue <devilcuruso@gmail.com>
2018-11-22 05:18:05 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,162 @@
+from flask import Flask, request, render_template, jsonify, abort
+from flask import make_response, url_for
+from flask_httpauth import HTTPBasicAuth
+from datetime import datetime
+
+app = Flask(__name__)
+
+#Authentication
+auth = HTTPBasicAuth()
+
+@auth.get_password
+def get_password(username):
+    if username == 'miguel':
+        return 'python'
+    return None
+
+@auth.error_handler
+def unauthorized():
+    return make_response(jsonify({'error': 'Unauthorized access'}), 403)
+
+#Restful API
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
+#GET Method
+@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@auth.login_required
+def get_tasks():
+        return jsonify({'tasks': [make_public_task(task) for task in tasks]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+#GET Method with ID
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
+#POST Method
+@app.route('/todo/api/v1.0/tasks', methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+        'id': tasks[-1]['id'] + 1,
+        'title': request.json['title'],
+        'description': request.json.get('description', ""),
+        'done': False
+    }
+    tasks.append(task)
+    return jsonify({'task': task}), 201
+
+#PUT Method
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    if not request.json:
+        abort(400)
+    if 'title' in request.json and type(request.json['title']) != unicode:
+        abort(400)
+    if 'description' in request.json and type(request.json['description']) is not unicode:
+        abort(400)
+    if 'done' in request.json and type(request.json['done']) is not bool:
+        abort(400)
+    task[0]['title'] = request.json.get('title', task[0]['title'])
+    task[0]['description'] = request.json.get('description', task[0]['description'])
+    task[0]['done'] = request.json.get('done', task[0]['done'])
+    return jsonify({'task': task[0]})
+
+#DELETE Method
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    tasks.remove(task[0])
+    return jsonify({'result': True})
+
+
+#Error Handler
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+def make_public_task(task):
+    new_task = {}
+    for field in task:
+        if field == 'id':
+            new_task['uri'] = url_for('get_task', task_id=task['id'], _external=True)
+        else:
+            new_task[field] = task[field]
+    return new_task
+
+
+##################################################
+
+#GET ONLY ROUTES
+# @ signifies a decorator - way to wrap a function and modify it's behavior
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/hello/<name>")
+def hello_there(name):
+    return render_template(
+        "hello_there.html",
+        name=name,
+        date=datetime.now()
+    )
+
+# New functions
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route('/shopping')
+def shopping():
+    food = ['Cheese', 'Tuna', 'Beef']
+    return render_template("shopping.html", food=food)
+
+@app.route('/profile/<name>')
+def profile(name):
+    return render_template("profile.html", name=name)
+
+@app.route('/product/<int:product_id>')
+def product(product_id):
+    return '<h2>Product ID is %s!</h2>' %product_id
+
+#POST
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return "Method used: %s" % request.method
+
+#Get data from the API
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
+
+if  __name__ == "__main__":
+    app.run(debug=True)
